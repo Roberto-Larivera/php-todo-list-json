fd1c01d1a1f1f512ab1e5e71d0b99424f018a168
Roberto Larivera <robylariv@gmail.com>
2023-02-23 23:19:11 +0100

    Aggiunte verifiche + ricerche tramite index

--- php/delete.php
+++ php/delete.php
@@ -2,29 +2,48 @@
 // RICHIESTA DATABASE
 $contentDatabase =  file_get_contents('../database/database.json');
 $contentDecode = json_decode($contentDatabase, true);
+$responseBol = false;
 
-
-$upgradeTodo = [
-    'todo' => $_POST['deleteTodo']['todo'],
-    'done' => filter_var($_POST['deleteTodo']['done'], FILTER_VALIDATE_BOOLEAN),
+$deleteTodoIndex = $_POST['deleteTodoIndex'] ?? null;
+$deleteTodo = [
+    'todo' => ($_POST['deleteTodo']['todo'] ?? null),
+    'done' => filter_var(($_POST['deleteTodo']['done']), FILTER_VALIDATE_BOOLEAN),
 ];
+if ($deleteTodoIndex !== null && $contentDecode[$deleteTodoIndex]['todo'] == $deleteTodo['todo']) {
+    unset($contentDecode[$deleteTodoIndex]);
+    $contentDecode = array_values($contentDecode);
+    $todoDelete = $contentDecode[$deleteTodoIndex];
+    $responseBol = true;
+} else {
 
-foreach($contentDecode as $index => $todo){
-    if ($todo['todo'] == $upgradeTodo['todo']){
-        unset($contentDecode[$index]);
-        $contentDecode = array_values($contentDecode);
-        $todoDelete = $todo;
-        break;
-    };
+    foreach ($contentDecode as $index => $todo) {
+        if ($todo['todo'] == $deleteTodo['todo']) {
+            unset($contentDecode[$index]);
+            $contentDecode = array_values($contentDecode);
+            $todoDelete = $todo;
+            $responseBol = true;
+
+            break;
+        } else {
+        };
+    }
 }
 
-file_put_contents('../database/database.json', json_encode($contentDecode) );
+if ($responseBol) {
+    $response = [
+        'success' => true,
+        'message' => 'OK - Delete todo',
+        'code' => 200,
+        'data' => $todoDelete
+    ];
+} else {
+    $response = [
+        'success' => true,
+        'message' => 'Error delete todo',
+        'code' => 400
+    ];
+}
 
-$response = [
-    'success' => true,
-    'message' => 'Delete todo OK',
-    'code' => 200,
-    'data' => $todoDelete
-];
 
-echo json_encode($response) ;
+file_put_contents('../database/database.json', json_encode($contentDecode));
+echo json_encode($response);
--- php/upgrade.php
+++ php/upgrade.php
@@ -2,38 +2,50 @@
 // RICHIESTA DATABASE
 $contentDatabase =  file_get_contents('../database/database.json');
 $contentDecoded = json_decode($contentDatabase, true);
+$responseBol = false;
 
-$upgradeTodoIndex = $_POST['upgradeTodoIndex'];
+
+$upgradeTodoIndex = $_POST['upgradeTodoIndex'] ?? null;
 $upgradeTodo = [
-    'todo' => $_POST['upgradeTodo']['todo'],
-    'done' => filter_var($_POST['upgradeTodo']['done'], FILTER_VALIDATE_BOOLEAN),
+    'todo' => ($_POST['upgradeTodo']['todo'] ?? null),
+    'done' => filter_var(($_POST['upgradeTodo']['done'] ?? null), FILTER_VALIDATE_BOOLEAN),
 ];
 
-if($contentDecoded[$upgradeTodoIndex]['todo'] == $upgradeTodo['todo']){
+if ($_POST['upgradeTodoIndex'] !== null && $contentDecoded[$upgradeTodoIndex]['todo'] == $upgradeTodo['todo']) {
 
     $contentDecoded[$upgradeTodoIndex]['done'] = !$contentDecoded[$upgradeTodoIndex]['done'];
-    $todoUpgrade = $todo['todo'];
+    $todoUpgrade = $contentDecoded[$upgradeTodoIndex]['todo'];
+    $responseBol = true;
+} else {
 
-}else{
-
-    foreach($contentDecoded as $index => $todo){
-        if ($todo['todo'] == $upgradeTodo['todo']){
+    foreach ($contentDecoded as $index => $todo) {
+        if ($todo['todo'] == $upgradeTodo['todo']) {
             //$todo['done'] = !$todo['done'];
             $contentDecoded[$index]['done'] = !$contentDecoded[$index]['done'];
             $todoUpgrade = $todo['todo'];
+            $responseBol = true;
+
             break;
         };
     }
 }
 
 
-file_put_contents('../database/database.json', json_encode($contentDecoded) );
+if ($responseBol) {
+    $response = [
+        'success' => true,
+        'message' => 'Upgrade todo OK',
+        'code' => 200,
+        'data' => $todoUpgrade
+    ];
+} else {
+    $response = [
+        'success' => true,
+        'message' => 'Error upgrade todo',
+        'code' => 400
+    ];
+}
 
-$response = [
-    'success' => true,
-    'message' => 'Upgrade todo OK',
-    'code' => 200,
-    'data' => $todoUpgrade
-];
 
-echo json_encode($response) ;
+file_put_contents('../database/database.json', json_encode($contentDecoded));
+echo json_encode($response);
